f2f004cdd78cbf18430c9c4a7cd1cf1fe638c76f
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-01 12:48:58 +0700

    Setup unit test

---
+++ DotCmisTests/src/test/java/bcircle/CmisTest.kt
@@ -0,0 +1,11 @@
+package bcircle
+
+import org.junit.Assert
+import org.junit.Test
+
+class CmisTest() {
+    @Test
+    fun simpleTest() {
+        Assert.assertEquals(2, 1 + 1)
+    }
+}
---
+++ DotCmisTests/src/test/java/bcircle/HelloKotlinKtTest.kt
@@ -0,0 +1,11 @@
+package bcircle
+
+import org.junit.Assert
+import org.junit.Test
+
+class HelloKotlinKtTest() {
+    @Test
+    fun simpleTest2() {
+        Assert.assertEquals(2, 1 + 1)
+    }
+}
