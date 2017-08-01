3e5f556a69962c13e0e709a4d56d1727cd7a0e7e
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-01 14:23:21 +0700

    Test change log

--- DotCmisTests/src/test/java/bcircle/OpenCmisTest.java
+++ DotCmisTests/src/test/java/bcircle/OpenCmisTest.java
@@ -1,33 +1,41 @@
 package bcircle;
 
-import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.ChangeEvent;
+import org.apache.chemistry.opencmis.client.api.ChangeEvents;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class OpenCmisTest {
-    @Test
-    public  void createSessionTest () {
+    public  Session createSession () {
         SessionFactory factory = SessionFactoryImpl.newInstance();
         Map<String, String> parameter = new HashMap<String, String>();
 
         parameter.put(SessionParameter.USER, "admin");
         parameter.put(SessionParameter.PASSWORD, "admin");
 
-        parameter.put(SessionParameter.ATOMPUB_URL, "http://192.168.0.109:8080/alfresco/api/-default-/public/cmis/versions/1.1/atom");
+        parameter.put(SessionParameter.ATOMPUB_URL, "http://192.168.0.20:8080/alfresco/api/-default-/public/cmis/versions/1.1/atom");
         parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
         parameter.put(SessionParameter.REPOSITORY_ID, "-default-");
 
         Session session = factory.createSession(parameter);
-        CmisObject object = session.getObjectByPath("/Validate");
-        Assert.assertEquals(object.getName(), "Validate");
+        return  session;
+    }
 
+    @Test
+    public  void checkLogTest() {
+       Session session = createSession();
+       ChangeEvents changes = session.getContentChanges(null, false, 100);
+       for(ChangeEvent change : changes.getChangeEvents()) {
+           System.out.println(change.getChangeType());
+           System.out.println(change.getObjectId());
+       }
+        System.out.println(changes.getLatestChangeLogToken());
     }
 }
